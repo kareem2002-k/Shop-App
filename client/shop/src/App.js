8e2fbf07e536f2f1d4d7d9b8a349a79df057c755
kareem2002-k <kareemsamir166@gmail.com>
2023-07-07 01:01:29 +0300

    Log in and Register Working
    Test Login and Register from front end end

--- client/shop/src/App.js
+++ client/shop/src/App.js
@@ -1,23 +1,111 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8000/login", {
+      email: email,
+      password: password
+    }, {
+      withCredentials: true
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8000/register", {
+      name: firstName+" "+lastName,
+      email: email,
+      password: password
+    }, {
+      withCredentials: true
+    })
+
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+
+    
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Log in</h1>
+      <form>
+        <label>
+          Email:
+          <input type="text" name="email" 
+          onChange={(e)=> setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password"
+            onChange={(e)=> setPassword(e.target.value)}
+          
+          />
+        </label>
+        <input type="submit" value="Log in" />
+        <button onClick={handleLogin}>Log in</button>
+      </form>
+
+      <h1>Register</h1>
+      <form>
+        <label>
+          First Name:
+          <input className=' text-lg  ' type="text" name="firstName"
+          onChange={(e)=> setFirstName(e.target.value)}
+          
+          />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" name="lastName"
+          onChange={(e)=> setLastName(e.target.value)}
+          
+          />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email"
+          onChange={(e)=> setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password"
+          onChange={(e)=> setPassword(e.target.value)}
+          
+          />
+        </label>
+
+        <button onClick={handleRegister}>Register</button>
+        </form>
+
+
     </div>
   );
 }
--- go-auth/main.go
+++ go-auth/main.go
@@ -9,20 +9,23 @@ import (
 )
 
 func main() {
-	// Connect to database
+	// Connect to the database
 	database.Connect()
 
-	// Create new fiber app
+	// Create a new Fiber app
 	app := fiber.New()
 
 	// CORS
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowOrigins:     "http://localhost:3000", // Replace with your allowed origin(s)
+
 	}))
 
 	// Routes
 	routes.Setup(app)
 
 	app.Listen(":8000")
-
 }
